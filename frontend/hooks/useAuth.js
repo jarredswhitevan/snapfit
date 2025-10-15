523c63287e15a9a24dee5dc434e195f413c27068
jarredswhitevan <129229855+jarredswhitevan@users.noreply.github.com>
2025-10-15 16:08:10 -0400

    Create useAuth.js

---
+++ frontend/hooks/useAuth.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+import { auth } from "../lib/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
+export default function useAuth() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, currentUser => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return { user, loading };
+}
